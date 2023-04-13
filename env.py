6a7082cdbbf50815a35f247d4cead8e91e087220
kartiksirop1 <125724209+kartiksirop1@users.noreply.github.com>
2023-04-13 10:42:57 +0530

    Update env.py

--- env.py
+++ env.py
@@ -6,9 +6,9 @@ load_dotenv()
 API_ID = os.getenv("API_ID", "14202110").strip()
 API_HASH = os.getenv("API_HASH", "45f3a3ac8effd88e42aeabe3cfe4f520").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "5537497510").split()))
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "5589515550").split()))
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "Its_Venom_family")
+MUST_JOIN = os.getenv("MUST_JOIN", "about_Kartikrajofficial")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
